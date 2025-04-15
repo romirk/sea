2439b27e0dd486f1b0333d22923cfdd0ef850b30
arya dradjica <kgvslxrya6nrasw9@bal-e.org>
2025-04-15 15:40:02 +0200

    Get the path to the source file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{env::args_os, path::PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    // The path to the source file.
+    let path: PathBuf = args_os()
+        .nth(1)
+        .unwrap_or_else(|| {
+            eprintln!("Usage: sea <path>");
+            std::process::exit(1)
+        })
+        .into();
+
+    println!("Reading from {}", path.display());
 }
